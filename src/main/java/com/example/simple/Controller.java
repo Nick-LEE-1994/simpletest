d9b917e0ac0c9b4fad332376a80846d01576e3f8
likeyu <likeyu@cmii.chinamobile.com>
2020-12-31 11:13:07 +0800

    change Controller

--- src/main/java/com/example/simple/Controller.java
+++ src/main/java/com/example/simple/Controller.java
@@ -3,6 +3,9 @@ package com.example.simple;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @description:
  * @author: likeyu
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/hello")
     public String hello(){
-        return "HELLO!";
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        String date = df.format(new Date());// new Date()为获取当前系统时间
+        return date;
     }
 }
