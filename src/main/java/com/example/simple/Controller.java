4b3dda1e7e99ce868857a160604a7de7e5a5dff8
likeyu <likeyu@cmii.chinamobile.com>
2021-01-05 16:35:51 +0800

    add :hello!

--- src/main/java/com/example/simple/Controller.java
+++ src/main/java/com/example/simple/Controller.java
@@ -16,6 +16,6 @@ public class Controller {
     @GetMapping("/hello")
     public String hello(){ SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         String date = df.format(new Date());// new Date()为获取当前系统时间
-        return date+":hello";
+        return date+":hello!";
     }
 }
